d17aa171ef96f520322959138594829a9d523639
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-11-29 16:29:26 -0300

    First commit

---
+++ gaia/src/pages/404.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const NotFoundPage = () => <div>Pagina no encontrada</div>
+
+export default NotFoundPage
---
+++ gaia/src/pages/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+// import { Link } from "gatsby"
+
+const IndexPage = () => <div>Hola amigos</div>
+
+export default IndexPage

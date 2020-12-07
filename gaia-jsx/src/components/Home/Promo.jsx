890dda66900a054b7189643ecb6cf0ba7e5e4f45
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-12-07 15:11:50 -0300

    Error on cols

--- gaia-jsx/src/components/Home/Promo.jsx
+++ gaia-jsx/src/components/Home/Promo.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { Link } from "react-router-dom";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+// import { Link } from "react-router-dom";
 
 export default function Promo() {
   return (
